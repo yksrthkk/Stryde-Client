469e331c3dc9832be13a8acc3690c45e0180ce04
सार्थK <decryptinginternet@gmail.com>
2025-11-18 21:43:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -156,9 +156,12 @@ window.addEventListener('scroll', function () {
     }
   });
 
-  const navToggle = document.getElementById("navToggle");
-const navLinks = document.querySelector(".nav-links");
+  cdocument.addEventListener("DOMContentLoaded", () => {
+    const navToggle = document.querySelector(".nav-toggle");
+    const navLinks = document.querySelector(".nav-links");
 
-navToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+    navToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
 });
+
